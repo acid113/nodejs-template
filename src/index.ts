65e26c80924dd15c0cd374c01361befca27f13be
Marvin Estacio <marvin.estacio@merchantspring.com.au>
2021-04-01 20:44:08 +0800

    boilerplate update

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
-import express from "express";
+import express, { Request, Response } from "express";
 
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Get success!");
+app.get("/", (req: Request, res: Response) => {
+  const date = new Date();
+  res.status(200).send({
+    message: date.toISOString(),
+  });
 });
 app.listen(port, () => {
   console.log("Express server started on port: " + port);
